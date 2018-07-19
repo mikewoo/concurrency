ef810bbc5e3870903d7984f29f19611bf46b3a5a
Eric Gui <ahqsgmm@163.com>
2018-07-20 00:05:45 +0800

    添加自定义注解

---
+++ src/main/java/com/mikewoo/study/concurrency/annotations/NotThreadSafe.java
@@ -0,0 +1,18 @@
+package com.mikewoo.study.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记【不线程安全】的类或者方法
+ * @author Eric Gui
+ * @date 2018/7/20
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+}
