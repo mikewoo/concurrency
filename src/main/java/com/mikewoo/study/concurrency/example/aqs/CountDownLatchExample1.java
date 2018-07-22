27e85ab6e05477b88df8bfeac5d7f48a612562ad
Eric Gui <ahqsgmm@163.com>
2018-07-22 20:57:00 +0800

    CountDownLatch使用实例

---
+++ src/main/java/com/mikewoo/study/concurrency/example/aqs/CountDownLatchExample1.java
@@ -0,0 +1,45 @@
+package com.mikewoo.study.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Eric Gui
+ * @date 2018/7/22
+ */
+@Slf4j
+public class CountDownLatchExample1 {
+
+    private final static int threadCount = 200;
+
+    public static void main(String[] args) throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            executorService.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (Exception e) {
+                    log.error("exception", e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+        log.info("finish");
+    }
+
+    private static void test(int threadNum) throws Exception {
+        Thread.sleep(100);
+        log.info("{}", threadNum);
+        Thread.sleep(100);
+    }
+}
